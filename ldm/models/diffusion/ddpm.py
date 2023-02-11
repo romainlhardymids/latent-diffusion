20b1c89d7b630da56511b4bcf997b7603fa57aba
romainlhardymids <86680046+romainlhardymids@users.noreply.github.com>
2023-02-11 03:43:08 -0800

    Enable first stage training

--- ldm/models/diffusion/ddpm.py
+++ ldm/models/diffusion/ddpm.py
@@ -501,10 +501,11 @@ class LatentDiffusion(DDPM):
 
     def instantiate_first_stage(self, config):
         model = instantiate_from_config(config)
-        self.first_stage_model = model.eval()
-        self.first_stage_model.train = disabled_train
-        for param in self.first_stage_model.parameters():
-            param.requires_grad = False
+        self.first_stage_model = model
+#         self.first_stage_model = model.eval()
+#         self.first_stage_model.train = disabled_train
+#         for param in self.first_stage_model.parameters():
+#             param.requires_grad = False
 
     def instantiate_cond_stage(self, config):
         if not self.cond_stage_trainable:
